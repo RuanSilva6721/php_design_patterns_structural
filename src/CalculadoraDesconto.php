9d97a09a39802b25643e11441f3fb5184780b8c0
Ruan Silva <ruansilva6721@gmail.com>
2023-05-30 12:42:53 -0300

    implementation of the facede design pattern

--- src/CalculadoraDesconto.php
+++ src/CalculadoraDesconto.php
@@ -8,13 +8,18 @@ use Ruan\DP\Descontos\SemDesconto;
 class CalcuradoraDesconto {
     public function calculaDesconto(Orcamento $orcamento){
 
+
+
         $cadeiraDesconto = new DescontoMaisDe5Itens(
             new DescontoMaior500(
                 new SemDesconto()
             )
             );
+            $descontoCalculado= $cadeiraDesconto->calcuaDesconto($orcamento);
+            $logDesconto = new LogDesconto();
+            $logDesconto->informar($descontoCalculado);
 
-            return $cadeiraDesconto->calcuaDesconto($orcamento);
+            return $descontoCalculado;
 
     }
 
---
+++ src/LogDesconto.php
@@ -0,0 +1,10 @@
+<?php
+namespace Ruan\DP;
+
+class LogDesconto{
+    public function informar(float $valor):void
+    {
+        echo "salvandolog do desconto";
+    }
+
+}
